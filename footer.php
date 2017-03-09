48832409c808130b881da146dde76ce84e29a5cc
daniellabonte <daniel0labonte@gmail.com>
2017-03-09 12:48:16 -0500

    header footer implimented(added the footer lel)

---
+++ footer.php
@@ -0,0 +1,12 @@
+			</div><!-- END OF CONTAINER -->
+		</div><!-- END OF MAIN -->
+	</div><!-- END OF WRAP -->
+
+	<div id="footer"> <!-- Footer in id=footer -->
+		<div class="container">
+			<p>FOOTER</p>
+		</div>
+	</div><!-- END OF FOOTER -->
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include 'header.php'; ?>
+</div><!-- Close container from header to make slider full browser width -->
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+		<!-- location for all the stored items -->
+		<div class = "carousel-inner">
+			<!-- Image must be right size -->
+				<div class="item active">
+					<img src="https://images2.alphacoders.com/700/70070.jpg">
+					<div class="carousel-caption">
+						<h2>Big City</h2>
+					</div>
+				</div>
+
+				<div class="item ">
+					<img src="https://newevolutiondesigns.com/images/freebies/city-wallpaper-18.jpg">
+				</div>
+
+				<div class="item ">
+					<img src="https://www.walldevil.com/wallpapers/a51/6115-wallpaper-cityscape-canada-city-toronto-images.jpg">
+				</div>
+
+			</div>
+
+		</div>
+	<div class="container"><!-- Reopen container for page contents -->
+
+	<!-- STUFF HERE -->
+
+<?php include 'footer.php'; ?>
