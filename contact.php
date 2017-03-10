8af7f26d3ace36c6dbff87d9611fb469f831a900
JosephLees <joey_lees@hotmail.com>
2017-03-09 20:08:46 -0500

    Set up most of contact pages

--- contact.php
+++ contact.php
@@ -1,15 +1,25 @@
 <?php include 'header.php'; ?>
-<center><h1>Contact</h1></center>
-<div class="col-sm-6">
-<input type="text" placeholder="Name" class="textbox" name="">
-<input type="text" placeholder="Email" class="textbox" name="">
-<center><h3>Inquery Type</h3></center>
-
-</div>
-<div class="col-sm-6">
-<textarea class="Comment" placeholder="Message"></textarea>
-</div>
-	
+<form name="ContactForm">
+	<center><h1>Contact</h1></center>
+	<div class="col-sm-6">
+		<input type="text" placeholder="Name" class="textbox" name="">
+		<input type="text" placeholder="Email" class="textbox" name="">
+		<center><h3>Inquery Type</h3></center>
+		<ul>
+		<div class="col-sm-6">
+			<li><input type="radio" name="Questions" value="Questions">Question</li>
+			<li><input type="radio" name="Questions" value="Comment">Comment</li>
+		</div>
+		<div class="col-sm-6">
+			<li><input type="radio" name="Questions" value="Concern">Concern</li>
+			<li><input type="radio" name="Questions" value="Shipping">Shipping</li>
+			</div>
+		</ul>
+	</div>
+	<div class="col-sm-6">
+		<textarea class="Comment" placeholder="Message"></textarea>
+	</div>
+</form>	
 <style type="text/css">
 	.textbox
 	{
@@ -21,6 +31,11 @@
 		width:100%;
 		margin: 10px;
 		height: 10em;
+	}
+
+	li
+	{
+		list-style-type: none;
 	}
 </style>
 <?php include 'footer.php'; ?>
